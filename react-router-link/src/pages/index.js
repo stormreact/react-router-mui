9498c615682513367ad5eca0b1321883ac028ee5
Michael Angerman <stormasm@gmail.com>
2018-01-09 13:24:32 -0800

    remove dialog

--- react-router-link/src/pages/index.js
+++ react-router-link/src/pages/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
-import Dialog, {
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-} from 'material-ui/Dialog';
 import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
 import withRoot from '../withRoot';
@@ -41,17 +35,6 @@ class Index extends React.Component {
 
     return (
       <div className={classes.root}>
-        <Dialog open={open} onClose={this.handleClose}>
-          <DialogTitle>Super Secret Password</DialogTitle>
-          <DialogContent>
-            <DialogContentText>1-2-3-4-5</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary" onClick={this.handleClose}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
         <Typography type="display1" gutterBottom>
           Material-UI
         </Typography>
