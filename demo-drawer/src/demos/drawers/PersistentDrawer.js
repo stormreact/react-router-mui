30aed717bdbc116c736e76340f330dbb013a347a
Michael Angerman <stormasm@gmail.com>
2018-01-10 10:18:55 -0800

    cc

--- demo-drawer/src/demos/drawers/PersistentDrawer.js
+++ demo-drawer/src/demos/drawers/PersistentDrawer.js
@@ -6,9 +6,7 @@ import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 
-//import { Link } from 'react-router-dom'
 import Button from 'material-ui/Button';
-//import { List, ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import { ListItem } from 'material-ui/List';
 
 import Typography from 'material-ui/Typography';
@@ -16,8 +14,6 @@ import Divider from 'material-ui/Divider';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
 import ChevronLeftIcon from 'material-ui-icons/ChevronLeft';
-//import ChevronRightIcon from 'material-ui-icons/ChevronRight';
-//import { mailFolderListItems, otherMailFolderListItems } from './tileData';
 
 const drawerWidth = 240;
 
