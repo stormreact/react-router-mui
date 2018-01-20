ef3f5230d9517e8971b11866e4567dec9398b03f
Michael Angerman <stormasm@gmail.com>
2018-01-19 18:59:29 -0800

    Example Sidebar is up and running

---
+++ react-router-examples/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import SidebarExample from './components/Sidebar';
+
+render((
+  <SidebarExample />
+), document.getElementById('root'));
