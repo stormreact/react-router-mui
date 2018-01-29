b7c1144525309f8388ee7fd768310237558bb025
Michael Angerman <stormasm@gmail.com>
2018-01-29 10:30:19 -0500

    withRouter1

---
+++ withRouter1/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import SidebarExample from './components/Sidebar2';
+
+render((
+  <SidebarExample />
+), document.getElementById('root'));
