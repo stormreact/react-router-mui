afc0089050455dac59ca00f0d6455007dcd47e73
Michael Angerman <stormasm@gmail.com>
2018-06-10 14:55:48 -0700

    MenuAppBarOrig

--- react-router-menus/src/index.js
+++ react-router-menus/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 //import App from './simpletabs';
-import App from './MenuAppBar';
+//import App from './MenuAppBar';
+import App from './MenuAppBarOrig';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
