7c053300695ab1c122454ef2c72d317475e7f73a
Michael Angerman <stormasm@gmail.com>
2018-06-10 14:29:45 -0700

    remove icon stuff

--- react-router-menus/src/MenuAppBar.js
+++ react-router-menus/src/MenuAppBar.js
@@ -4,9 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
@@ -61,22 +58,14 @@ class MenuAppBar extends React.Component {
         </FormGroup>
         <AppBar position="static">
           <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
             <Typography variant="title" color="inherit" className={classes.flex}>
               Title
             </Typography>
             {auth && (
               <div>
-                <IconButton
-                  aria-owns={open ? 'menu-appbar' : null}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
+                <Typography>
+                  Menu
+                </Typography>
                 <Menu
                   id="menu-appbar"
                   anchorEl={anchorEl}
--- react-router-menus/src/index.js
+++ react-router-menus/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 //import App from './simpletabs';
+import App from './MenuAppBar';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
