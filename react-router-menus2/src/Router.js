d0d3d091caa1873532b5b12ec47bdb55eeea57f2
Michael Angerman <stormasm@gmail.com>
2018-06-18 11:16:32 -0700

    AppMenu

--- react-router-menus2/src/Router.js
+++ react-router-menus2/src/Router.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, withRouter } from "react-router-dom";
 
-import Nav from "./AppTab";
+//import Nav from "./AppTab";
+import Nav from "./AppMenu";
 const MyNav = withRouter(Nav);
 
 const NavTop = () => (
